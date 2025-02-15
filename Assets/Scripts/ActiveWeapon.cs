a90a382b02e3c5bb9d7816fcee717718a68da525
Daniel <danjfreire@gmail.com>
2025-02-15 08:58:23 -0300

    feat: refactor weapon and add fire rate cd

---
+++ Assets/Scripts/ActiveWeapon.cs
@@ -0,0 +1,41 @@
+using StarterAssets;
+using UnityEngine;
+
+public class ActiveWeapon : MonoBehaviour
+{
+    const string SHOOT_ANIM = "Shoot";
+
+    [SerializeField] private WeaponSO weaponSO;
+
+    private Animator animator;
+    private Weapon currentWeapon;
+    private StarterAssets.StarterAssetsInputs inputs;
+    private float currentWeaponCd = 0f;
+
+    private void Start()
+    {
+        inputs = GetComponentInParent<StarterAssets.StarterAssetsInputs>();
+        animator = GetComponent<Animator>();
+        currentWeapon = GetComponentInChildren<Weapon>();
+    }
+
+    private void Update()
+    {
+        ShootWeapon();
+
+        if (currentWeaponCd > 0)
+        {
+            currentWeaponCd -= Time.deltaTime;
+        }
+    }
+
+    private void ShootWeapon()
+    {
+        if (!inputs.shoot || currentWeaponCd > 0) return;
+
+        animator.Play(SHOOT_ANIM, 0, 0f);
+        currentWeapon.Shoot(weaponSO);
+        currentWeaponCd = weaponSO.FireRate;
+        inputs.ShootInput(false);
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,34 +1,13 @@
-using StarterAssets;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] private WeaponSO weaponSO;
-    [SerializeField] private Animator animator;
-    [SerializeField] private ParticleSystem weaponHitFx;
     [SerializeField] private ParticleSystem muzzleFlash;
 
-    const string SHOOT_ANIM = "Shoot";
 
-    StarterAssets.StarterAssetsInputs inputs;
-
-    private void Start()
+    public void Shoot(WeaponSO weaponSO)
     {
-        inputs = GetComponentInParent<StarterAssets.StarterAssetsInputs>();
-    }
-
-    private void Update()
-    {
-        ShootWeapon();
-    }
-
-    private void ShootWeapon()
-    {
-        if (!inputs.shoot) return;
-
         muzzleFlash.Play();
-        animator.Play(SHOOT_ANIM, 0, 0f);
         RaycastHit hit;
 
         bool hasCollided = Physics.Raycast(
@@ -45,9 +24,7 @@ public class Weapon : MonoBehaviour
             enemyHealth?.TakeDamage(weaponSO.Damage);
 
             // Show the weapon hit effect
-            Instantiate(weaponHitFx, hit.point, quaternion.identity);
+            Instantiate(weaponSO.HitVfx, hit.point, Quaternion.identity);
         }
-
-        inputs.ShootInput(false);
     }
 }
