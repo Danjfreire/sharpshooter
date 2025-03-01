3b4a19e3f7a7c67c460320752bdc2c7bcfb1d4ba
Daniel <danjfreire@gmail.com>
2025-03-01 10:09:54 -0300

    feat: add transition to game over camera on player death

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,10 +1,14 @@
+using Cinemachine;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 5;
+    [SerializeField] private CinemachineVirtualCamera gameOverCam;
+    [SerializeField] Transform weaponCamera;
 
     private int currentHealth;
+    private int gameOverCameraPriority = 20;
 
     private void Awake()
     {
@@ -18,6 +22,10 @@ public class PlayerHealth : MonoBehaviour
 
         if (currentHealth <= 0)
         {
+            // When the player dies we need to remove the parent of the weapon camera, because it is a child of the player
+            // We also need to set the game over camera to a higher priority, so it automatically transitions to the game over camera
+            weaponCamera.parent = null;
+            gameOverCam.Priority = gameOverCameraPriority;
             Destroy(gameObject);
         }
     }
